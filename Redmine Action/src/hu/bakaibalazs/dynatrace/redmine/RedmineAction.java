398875619e9ac4a0bd8be3e878c33da62631b6b9
Balázs Bakai <bakaibalazs@gmail.com>
2015-09-04 11:31:22 +0200

    action

--- Redmine Action/src/hu/bakaibalazs/dynatrace/redmine/RedmineAction.java
+++ Redmine Action/src/hu/bakaibalazs/dynatrace/redmine/RedmineAction.java
@@ -1,7 +1,6 @@
 
 package hu.bakaibalazs.dynatrace.redmine;
 
-import java.util.List;
 import java.util.logging.Logger;
 
 import com.dynatrace.diagnostics.pdk.Action;
@@ -10,12 +9,10 @@ import com.dynatrace.diagnostics.pdk.Incident;
 import com.dynatrace.diagnostics.pdk.Status;
 import com.dynatrace.diagnostics.pdk.Violation;
 import com.taskadapter.redmineapi.IssueManager;
-import com.taskadapter.redmineapi.ProjectManager;
 import com.taskadapter.redmineapi.RedmineManager;
 import com.taskadapter.redmineapi.RedmineManagerFactory;
 import com.taskadapter.redmineapi.bean.Issue;
 import com.taskadapter.redmineapi.bean.IssueFactory;
-import com.taskadapter.redmineapi.bean.ProjectFactory;
 
 public class RedmineAction implements Action {
 
@@ -40,15 +37,17 @@ public class RedmineAction implements Action {
 			logIncidents(i);
 
 			RedmineManager mgr = RedmineManagerFactory.createWithApiKey(redmineServerURL, apiAccessKey);
-			IssueManager issueManager = mgr.getIssueManager();				
-			Issue issueToCreate = IssueFactory.create(mgr.getProjectManager().getProjectByKey(projectKey).getId(), issueSubjectPrefix+" "+i.getKey().getSystemProfile());		
-			issueToCreate.setDescription(i.getMessage());			
-			Issue createdIssue = issueManager.createIssue(issueToCreate);
-
+			IssueManager issueManager = mgr.getIssueManager();
+			Issue issueToCreate = IssueFactory.create(mgr.getProjectManager().getProjectByKey(projectKey).getId(),
+					issueSubjectPrefix + " " + i.getKey().getSystemProfile());
+			issueToCreate.setDescription(i.getMessage());
+			issueToCreate.setPriorityId(getRedminePriority(i));
+			issueManager.createIssue(issueToCreate);
+			log.info("Issue was created in Redmine");
 		}
 
 		return new Status(Status.StatusCode.Success);
-	}
+	} 
 
 	private void logIncidents(Incident incident) {
 		log.fine("RULE:" + incident.getIncidentRule().getName());
@@ -61,9 +60,40 @@ public class RedmineAction implements Action {
 			log.info("Measure " + violation.getViolatedMeasure().getName() + " violoated threshold.");
 		}
 	}
+	
+	private int getRedminePriority(Incident incident) {
+		Incident.Severity localSeverity = incident.getSeverity();
+
+		if (localSeverity == Incident.Severity.Error) // Redmine High == 3
+			return 3;
+		if (localSeverity == Incident.Severity.Warning) // Redmine Normal == 2
+			return 2;
+		if (localSeverity == Incident.Severity.Informational) { // Redmine Low == 1
+			return 1;
+		}
+		return 1;
+	}
 
 	@Override
 	public void teardown(ActionEnvironment env) throws Exception {
 		log.fine("Redmine Action tear down");
 	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		String uri = "http://hibajegy.kh.hu";
+	    String apiAccessKey = "3b72ae72c583f28b83c10b124d47928388db22c1";
+	    String projectKey = "ci-nlr-teszt";
+	    Integer queryId = null; 
+
+	    RedmineManager mgr = RedmineManagerFactory.createWithApiKey(uri, apiAccessKey);
+	    IssueManager issueManager = mgr.getIssueManager();
+	    java.util.List<Issue> issues = issueManager.getIssues(projectKey, queryId);
+	    for (Issue issue : issues) {
+	        System.out.println(issue.getSubject()+" - "+issue.getPriorityId());
+	    }
+
+	}
+	
+	
 }
